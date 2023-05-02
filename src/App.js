2947b20de5e877a04644a6b57af8d7ba44dbe153
selim ben mrad <benmradselim@gmail.com>
2023-05-02 12:31:46 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const AppTerms = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Terms of Use</h1>
+      <p>The app is intended to be used only by customers. Any other use that involves copying or using the content is strictly prohibited.</p>
+
+      <h2>Privacy Policy</h2>
+      <p>Data shared will be used only for internal purposes, such as delivery efficiency and menu adjustments. The data will not be sold to a third party in any manner.</p>
+
+      <p>For tracking our advertising strategy, we use a third-party provider. Click here to view more information on the website of Yandex: <a href="https://yandex.com/legal/confidential/">https://yandex.com/legal/confidential/</a></p>
+
+      <p>For authentication and databases, we use Firebase and Facebook. Click here to view more information on their websites: <a href="https://firebase.google.com/terms">https://firebase.google.com/terms</a> and <a href="https://developers.facebook.com/docs/legal/terms">https://developers.facebook.com/docs/legal/terms</a></p>
     </div>
   );
-}
+};
 
-export default App;
+export default AppTerms;
